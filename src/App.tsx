31178da8f03b5dd8a350319325a7d9b7535264e2
Inam-urRehman <mindgripper.developement@gmail.com>
2022-07-18 15:32:24 +0500

    Boiler plate for the chrome extension by using react and typescript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      This is the boiler plate for the interactive chrom extension with react and typescript
+    </div>
+  );
+}
+
+export default App;
